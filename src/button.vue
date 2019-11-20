4892c50218b4a9936bd9c04e02740e46e5a4c9e7
LeeBoo1995 <731420476@qq.com>
2019-11-21 00:28:25 +0800

    添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,18 +1,32 @@
 <template>
-  <button class="g-button" @click="handler">按钮1</button>
+  <button class="g-button"
+          :class="{[`icon-${iconPosition}`]:true}"
+          @click="handler">
+    <svg class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+
+   <div class="content">
+      <slot></slot>
+   </div>
+  </button>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-
+  props: {
+    icon: {
+      type: String
+    },
+    iconPosition: {
+      type: String
     }
   },
-  methods:{
-    handler(){
-      alert(1)
-    }
+  data() {
+    return {};
+  },
+  methods: {
+    handler() {}
   }
 };
 </script>
@@ -21,20 +35,39 @@ export default {
 .g-button {
   height: var(--button-height);
   padding: 0 1em;
+  vertical-align: middle;
   font: inherit;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
   &:hover {
     border-color: var(--border-color-hover);
   }
-
   &:active {
     background: var(--button-active-bg);
   }
-
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.3em;
+    }
   }
 }
 </style>
