422097581341e1e7aca8f38cbf14135904d8ec3c
LeeBoo1995 <731420476@qq.com>
2019-11-20 17:54:55 +0800

    add parcel

---
+++ src/button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="g-button" @click="handler">按钮</button>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    handler(){
+      alert(1)
+    }
+  }
+};
+</script>
+
+<style lang='scss'>
+.g-button {
+  height: var(--button-height);
+  padding: 0 1em;
+  font: inherit;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
