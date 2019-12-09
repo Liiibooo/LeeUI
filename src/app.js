7f5ec2406445c6ae2ca41b5854fa16d24b8de14c
LeeBoo1995 <731420476@qq.com>
2019-12-09 21:48:12 +0800

    添加单元测试

--- src/app.js
+++ src/app.js
@@ -8,6 +8,47 @@ new Vue({
     data: {
         isLoading1: false,
         isLoading2: false,
-        isLoading3:false
+        isLoading3: false
     }
 })
+import chai from 'chai'
+const expect = chai.expect
+//单元测试
+{
+    const Constructor = Vue.extend(Button) //创造一个子类
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    }) //生成一个button实例
+    button.$mount('#test')
+    let useElement = button.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+}
+{
+    const Constructor = Vue.extend(Button) //创造一个子类
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button) //创造一个子类
+    const button = new Constructor({
+        propsData: {
+            icon: 'love',
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+}
