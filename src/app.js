6a112aac018730a5f1e68e66e87a1be6e8004089
LeeBoo1995 <731420476@qq.com>
2019-12-13 23:42:28 +0800

    add input component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './l-icon.vue'
+import Input from './input.vue'
 Vue.component('l-icon', Icon)
 Vue.component('g-button', Button)
+Vue.component('l-input',Input)
 new Vue({
     el: "#app",
     data: {
@@ -14,41 +16,41 @@ new Vue({
 import chai from 'chai'
 const expect = chai.expect
 //单元测试
-{
-    const Constructor = Vue.extend(Button) //创造一个子类
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    }) //生成一个button实例
-    button.$mount('#test')
-    let useElement = button.$el.querySelector('use')
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
-}
-{
-    const Constructor = Vue.extend(Button) //创造一个子类
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-            loading: true
-        }
-    })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-}
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button) //创造一个子类
-    const button = new Constructor({
-        propsData: {
-            icon: 'love',
-        }
-    })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-}
+// {
+//     const Constructor = Vue.extend(Button) //创造一个子类
+//     const button = new Constructor({
+//         propsData: {
+//             icon: 'settings'
+//         }
+//     }) //生成一个button实例
+//     button.$mount('#test')
+//     let useElement = button.$el.querySelector('use')
+//     expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+// }
+// {
+//     const Constructor = Vue.extend(Button) //创造一个子类
+//     const button = new Constructor({
+//         propsData: {
+//             icon: 'settings',
+//             loading: true
+//         }
+//     })
+//     button.$mount()
+//     let useElement = button.$el.querySelector('use')
+//     let href = useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#i-loading')
+// }
+// {
+//     const div = document.createElement('div')
+//     document.body.appendChild(div)
+//     const Constructor = Vue.extend(Button) //创造一个子类
+//     const button = new Constructor({
+//         propsData: {
+//             icon: 'love',
+//         }
+//     })
+//     button.$mount(div)
+//     let svg = button.$el.querySelector('svg')
+//     let { order } = window.getComputedStyle(svg)
+//     expect(order).to.eq('1')
+// }
